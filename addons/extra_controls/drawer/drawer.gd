3a444423710d3d8f29ef5f976accc6cd97fa5a0c
Matthijs Mud <matthijsmud@gmail.com>
2025-02-04 16:05:59 +0100

    Apply padding to positioning

--- addons/extra_controls/drawer/drawer.gd
+++ addons/extra_controls/drawer/drawer.gd
@@ -1,6 +1,7 @@
 @icon("./icon.svg")
 @tool
 extends CustomContainer
+class_name Drawer
 
 const CustomContainer := preload("../_shared/custom_container.gd");
 
@@ -28,8 +29,8 @@ var _cached_drawer_panel: StyleBox = null:
 	
 func _update_theme_cache() -> void:
 	super();
-	_cached_drawer_panel = get_theme_stylebox("panel");
-	_cached_backdrop_color = get_theme_color("backdrop");
+	_cached_drawer_panel = get_theme_stylebox(&"panel", &"Drawer");
+	_cached_backdrop_color = get_theme_color(&"backdrop", &"Drawer");
 #endregion
 
 ## Value in range (0.0, 1.0). Open is represented by 1, closed by 0.
@@ -45,7 +46,16 @@ func close() -> void:
 
 var _padding: Vector2:
 	get(): return _cached_drawer_panel.get_minimum_size() if _cached_drawer_panel else Vector2.ZERO;
-	
+
+var _padding_left: float:
+	get(): return _cached_drawer_panel.content_margin_left if _cached_drawer_panel else 0.0;
+var _padding_right: float:
+	get(): return _cached_drawer_panel.content_margin_right if _cached_drawer_panel else 0.0;
+var _padding_top: float:
+	get(): return _cached_drawer_panel.content_margin_top if _cached_drawer_panel else 0.0;
+var _padding_bottom: float:
+	get(): return _cached_drawer_panel.content_margin_bottom if _cached_drawer_panel else 0.0;
+
 func _enter_tree() -> void:
 	_backdrop_id = RenderingServer.canvas_item_create();
 	RenderingServer.canvas_item_set_parent(_backdrop_id, get_canvas_item());
@@ -68,7 +78,7 @@ func _draw_backdrop() -> void:
 	RenderingServer.canvas_item_add_rect(_backdrop_id, Rect2(Vector2.ZERO, size), effective_color);
 
 func _draw_drawer() -> void:
-	draw_style_box(_cached_drawer_panel, _calculate_content_area());
+	draw_style_box(_cached_drawer_panel, _calculate_drawer_area());
 #endregion
 
 func _get_minimum_size() -> Vector2:
@@ -91,21 +101,31 @@ func _sort_children() -> void:
 		
 		fit_child_in_rect(control, content_area);
 
+func _calculate_drawer_area() -> Rect2:
+	var offset := _calculate_content_area_offset();
+	var size := _calculate_content_area_size();
+	return Rect2(offset, size);
+
 func _calculate_content_area() -> Rect2:
 	var size = _calculate_content_area_size();
 	var offset = _calculate_content_area_offset();
 	
-	return  Rect2(offset, size);
+	return  Rect2(offset, size).grow_individual(
+		-_padding_left,
+		-_padding_top,
+		-_padding_right,
+		-_padding_bottom
+	);
 
 func _calculate_content_area_offset() -> Vector2:
 	var content_size := get_combined_minimum_size();
 	var available_size := size;
 	
 	match side:
-		SIDE_LEFT:   return Vector2(-content_size.x * (1.0 - _openess), 0);
-		SIDE_RIGHT:  return Vector2(available_size.x - content_size.x * _openess, 0);
-		SIDE_TOP:    return Vector2(0, -content_size.y * (1.0 - _openess));
-		SIDE_BOTTOM: return Vector2(0, available_size.y - content_size.y * _openess);
+		SIDE_LEFT:   return Vector2(-content_size.x * (1.0 - _openess), 0.0);
+		SIDE_RIGHT:  return Vector2(available_size.x - content_size.x * _openess, 0.0);
+		SIDE_TOP:    return Vector2(0.0, -content_size.y * (1.0 - _openess));
+		SIDE_BOTTOM: return Vector2(0.0, available_size.y - content_size.y * _openess);
 	return Vector2.ZERO;
 
 func _calculate_content_area_size() -> Vector2:
